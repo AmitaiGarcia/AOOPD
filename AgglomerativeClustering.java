bf9a0a59e9fd1d1a2e05c7fa380e8cfd6043328d
Bryan Altman <brynaltman@gmail.com>
2021-11-18 19:09:52 +0200

    pushed broken code, good luck

--- AgglomerativeClustering.java
+++ AgglomerativeClustering.java
@@ -1,13 +1,32 @@
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Optional;
 import java.util.Set;
+import java.util.SimpleTimeZone;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.stream.*;
 
-
-public class AgglomerativeClustering <T extends Clusterable<T>> implements Clustering<T>{
+public class AgglomerativeClustering<T extends Clusterable<T>> implements Clustering<T> {
 	double threshold;
+
 	public AgglomerativeClustering(double threshold) {
 		this.threshold = threshold;
 	}
+
 	public Set<Set<T>> clusterSet(Set<T> elements) {
-		// TODO: Complete
-		return null;
+		while (elements.size() != 1) {
+
+			Optional<SimpleEntry<T, T>> closestPair = elements.stream()
+					.flatMap(elem -> elements.stream().filter(other -> !elem.equals(other))
+							.map(other -> new AbstractMap.SimpleEntry<T, T>(elem, other)))
+					.min(Comparator.comparingDouble(e -> ((T) e.getKey()).distance((T) e.getValue())));
+
+			if (closestPair.getKey().distance(closestPair.getValue()) > threshold) {
+				return elements;
+			}
+
+		}
 	}
 }
